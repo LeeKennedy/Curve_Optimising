528bec55b3942b9aaface41bceb264b5f8f6ecc6
LeeKennedy <mail@leekennedy.id.au>
2022-02-20 20:21:33 +1100

    Initial play

---
+++ scripts/fit.table.R
@@ -0,0 +1,63 @@
+#### Clean Up environment -----------------------------
+rm(list=ls())
+
+#### Packages -----------------------------
+library(readxl)
+library(tidyverse)
+library(LK.Toolbox)
+library(here)
+
+
+#### Functions -----------------------------
+
+
+#### Data Input -----------------------------
+here::here()
+
+data_in <- read_excel("data/Ethanol.xlsx")
+
+#### Data Cleaning -----------------------------
+
+n <- nrow(data_in)
+
+df <- data.frame(Run_set = as.character(),
+                 Slope = as.numeric(), 
+                 Intercept = as.numeric(), 
+                 R2 = as.numeric(),
+                 SE = as.numeric(),
+                 stringsAsFactors=FALSE) 
+m=n-3
+
+for(i in 1:m) {
+   
+        data_in2 <- data_in[c(i:n),]
+        
+        fit <- lm(data_in2$Ratio~data_in2$Standard)
+        fit_summary <- summary(fit)
+        slope_fit <- fit_summary$coefficients[2]
+        slope_intercept <- fit_summary$coefficients[1]
+        R2_fit <- fit_summary$r.squared
+        SE_fit <- fit_summary$sigma
+        Run_Set = paste(i,":",n)
+
+df <- rbind(df, data.frame(Run_set = Run_Set, Slope = slope_fit, Intercept = slope_intercept, R2 = R2_fit, SE = SE_fit))
+}
+df
+
+m=n-3
+
+for(i in 1:m) {
+        j=i+2
+        data_in2 <- data_in[c(1:j),]
+        
+        fit <- lm(data_in2$Ratio~data_in2$Standard)
+        fit_summary <- summary(fit)
+        slope_fit <- fit_summary$coefficients[2]
+        slope_intercept <- fit_summary$coefficients[1]
+        R2_fit <- fit_summary$r.squared
+        SE_fit <- fit_summary$sigma
+        Run_Set = paste("1:",j)
+        
+        df <- rbind(df, data.frame(Run_set = Run_Set, Slope = slope_fit, Intercept = slope_intercept, R2 = R2_fit, SE = SE_fit))
+}
+df
